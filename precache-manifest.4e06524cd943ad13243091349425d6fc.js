7be007d1a5052a60cdfbf94a92e61362cadc2585
Binay Kumar Sahoo <binay.sahoo2013@gmail.com>
2020-08-04 16:12:13 +0530

    Updates

---
+++ precache-manifest.4e06524cd943ad13243091349425d6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbb9ad5992ff3f3fd549512d0fce8b3",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "b5cd2fdf6ce05ef9bc08",
+    "url": "/firegram/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "47009bb72e3a16741577",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5cd2fdf6ce05ef9bc08",
+    "url": "/firegram/static/js/main.36031316.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
